3fb90b3065253045ba2d1bcdb63d69cf7a208184
LTP <lamphat279@gmail.com>
2025-01-05 20:55:14 +0700

    more skill

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,4 +1,11 @@
-const skills = [
+import { useEffect, useState } from "react";
+
+interface Skill {
+  title: string;
+  items: (string | { label: string; value: number })[];
+}
+
+const skills: Skill[] = [
   {
     title: "Languages",
     items: ["C++", "Java", "C#", "Python", "JavaScript", "TypeScript"],
@@ -15,22 +22,78 @@ const skills = [
     title: "Tools",
     items: ["Git", "VSCode", "Figma", "Docker", "Google Cloud Platform"],
   },
+  {
+    title: "Experience",
+    items: [
+      { label: "Lines of Code", value: 73000 },
+      { label: "Projects", value: 80 },
+      { label: "Papers", value: 2 },
+      { label: "Achievements", value: 10 },
+    ],
+  },
 ];
 
 export const Skills = () => {
+  const [counters, setCounters] = useState<{ [key: string]: number }>({});
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounters((prevCounters) => {
+        const updatedCounters = { ...prevCounters };
+        const experience = skills.find((skill) => skill.title === "Experience");
+
+        experience?.items.forEach((item) => {
+          if (typeof item === "object" && item.label) {
+            if (!updatedCounters[item.label]) updatedCounters[item.label] = 0;
+            if (updatedCounters[item.label] < item.value) {
+              updatedCounters[item.label] += 100; // Tăng 5 mỗi lần
+              if (updatedCounters[item.label] > item.value) {
+                updatedCounters[item.label] = item.value; // Đảm bảo không vượt quá giá trị mục tiêu
+              }
+            }
+          }
+        });
+
+        return updatedCounters;
+      });
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
       <div className="flex flex-col items-center justify-center gap-3">
         {skills.map((skill) => (
           <div key={skill.title}>
             <p className="text-center text-sm font-bold">{skill.title}</p>
-            <ul className="mt-1 flex flex-wrap justify-center gap-2 text-xs">
-              {skill.items.map((item) => (
-                <li key={item} className="rounded-lg bg-yellow p-1 text-black">
-                  {item}
-                </li>
-              ))}
-            </ul>
+            {skill.title === "Experience" ? (
+              <ul className="mt-1 flex gap-1 text-xs">
+                {skill.items.map((item) =>
+                  typeof item === "object" ? (
+                    <li key={item.label} className="text-center pr-5">
+                      {item.label}:{" "}
+                      <span className="font-bold flex flex-col text-yellow-600">
+                        {counters[item.label] || 0}
+                      </span>
+                    </li>
+                  ) : null
+                )}
+              </ul>
+            ) : (
+              <ul className="mt-1 flex flex-wrap justify-center gap-2 text-xs">
+                {skill.items.map((item) =>
+                  typeof item === "string" ? (
+                    <li
+                      key={`${skill.title}-${item}`}
+                      className="rounded-lg bg-yellow p-1 text-black"
+                    >
+                      {item}
+                    </li>
+                  ) : null
+                )}
+              </ul>
+            )}
           </div>
         ))}
       </div>
